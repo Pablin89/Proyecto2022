07344354bc16fa47591d3bc685a1cb811e5672b2
QUE HAY <QUE HAY@DESKTOP-I9BCHO8>
2022-10-14 11:53:50 -0300

    Borrado de Checkbox

--- Principal/Presentacion/Usuario_Gerente/ReporteSucursalIndividual.vb
+++ Principal/Presentacion/Usuario_Gerente/ReporteSucursalIndividual.vb
@@ -11,40 +11,7 @@
         End If
     End Sub
 
-    Private Sub CHfecha_CheckedChanged(sender As Object, e As EventArgs)
-        If CHfecha.Checked = True Then
-            CHmes.Checked = False
-            DateDesde.Enabled = True
-            DateHasta.Enabled = True
-            CBcategoria.Enabled = True
-            CBproducto.Enabled = True
-            CHfecha.Font = New Font(CHfecha.Font.Bold, 13)
-        Else
-            DateDesde.Enabled = False
-            DateHasta.Enabled = False
-            CBcategoria.Enabled = False
-            CBproducto.Enabled = False
-            CHfecha.Font = New Font(CHfecha.Font.Name, 12)
-        End If
-    End Sub
 
-    Private Sub CHmes_CheckedChanged(sender As Object, e As EventArgs)
-        If CHmes.Checked = True Then
-            CHfecha.Checked = False
-            CBcategoria.Enabled = True
-            CBproducto.Enabled = True
-            CBmes.Enabled = True
-            CBaño.Enabled = True
-            CHmes.Font = New Font(CHmes.Font.Bold, 13)
-
-        Else
-            CBmes.Enabled = False
-            CBaño.Enabled = False
-            CBcategoria.Enabled = False
-            CBproducto.Enabled = False
-            CHmes.Font = New Font(CHmes.Font.Name, 12)
-        End If
-    End Sub
 
     Private Sub ReportesSucursalIndividual_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
