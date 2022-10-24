a9b68bf1df0e7f7d93b0337d28db7f07fe9d9d55
QUE HAY <QUE HAY@DESKTOP-I9BCHO8>
2022-10-24 19:00:42 -0300

    Inicio de Sesión incorrecto

--- Principal/Principal.vb
+++ Principal/Principal.vb
@@ -33,16 +33,25 @@
     End Sub
 
     Private Sub BSesion_Click(sender As Object, e As EventArgs) Handles BSesion.Click
-        Me.Hide()
+
+
         If TUsuario.Text = "Encargado" Then
+            Me.Hide()
             Encargado.Show()
         ElseIf TUsuario.Text = "Gerente" Then
+            Me.Hide()
             Gerente.Show()
         ElseIf TUsuario.Text = "Cajero" Then
+            Me.Hide()
             Cajeros.Show()
         ElseIf TUsuario.Text = "GerenteGral" Then
+            Me.Hide()
             GerenteGral.Show()
+        Else
+            MsgBox("El usuario o contraseña ingresados son incorrectos. Intente nuevamente o pongase en contacto con el administrador del sistema.", MsgBoxStyle.Critical, "Problema de inicio de sesión")
+
         End If
+
 
     End Sub
 End Class
